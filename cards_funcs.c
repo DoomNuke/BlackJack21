fb11670aca6b2ecd20f7c3a81e71bb709b6561c1
DoomNuke <danielpier2402@gmail.com>
2024-09-23 23:58:17 +0300

    This is an update of the functions for cards functions,

---
+++ cards_funcs.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+
+#include "cards_funcs.h"
+//Initializing an empty list of cards//
+Cards_List_init(Cards_List *cards);
+//Freeing the allocated cards each round//
+Cards_List_Free(Cards_List *cards){
+    if (cards->length > 0){
+        free(Cards_List)
+    }
+};
--- cards_funcs.h
+++ cards_funcs.h
@@ -1,5 +1,58 @@
 #ifndef CARDS_FUNC_H
 #define CARDS_FUNC_H
 
-#include <stdlib.h>
 #include "cards.h"
+
+// Initializing an empty card list - deck, player,dealer//
+void Cards_List_init(Cards_List *cards)
+{
+    cards->length = 0;
+    cards->head = NULL;
+    cards-> tail = NULL;
+}
+
+
+void Cards_Add (Cards_List *cards, Card *m_card)
+{
+    if(cards -> length == 0){
+        cards->head = m_card;
+        cards->tail = m_card;
+        cards->length = 1;
+    }
+    else
+    {
+        cards -> tail -> next = m_card;
+        cards->tail = m_card;
+        cards->length++;
+    }
+}
+
+Card * Card_Remove(Cards_List *cards){
+    if (cards->length == 0) return NULL;
+    {
+        Card *off_card = cards->head;
+        cards->length--;
+    }
+    if (cards->length == 0){
+        cards->head = NULL;
+        cards->tail = NULL;
+    }
+
+    else
+    //y u no work TODO: commit arson om C's official language creator//
+    //                     ||                       //
+    //                     ||                       //
+    //                     \/                       //
+    {
+        cards -> head = off_card -> 
+    }
+}
+
+void Card_List_Off(Cards_List *cards)
+{
+    while (cards -> length > 0){
+        free(Card_Remove(cards));
+    }
+}
+
+#endif 
